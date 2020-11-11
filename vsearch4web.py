57ea151b2c413608fd8af72f31217f97d9a241e5
Diogo Rodrigues <diogo.borbar@gmail.com>
2020-11-11 20:06:07 -0300

    aula 10 SI

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from vogais_aula6 import proLetras
 app = Flask (__name__)
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-@app.route('/search4')
-def do_search() ->str:
-    return str(proLetras('life, the universe, and everything', 'eiru!'))
+def hello() -> '302':
+    return redirect('/entry')
+
+@app.route('/search4', methods = ['POST'])
+def do_search() ->'htlm':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results'
+    results = str(proLetras(phrase, letters))
+    return render_template('results.html', the_phrase = phrase,
+    the_letters = letters, 
+    the_title = title,
+    the_results = results,)
 
 @app.route('/entry')
 def entry_page() ->'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
-
-app.run()
+app.run(debug=True)
